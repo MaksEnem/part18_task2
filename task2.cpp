0103ea0801faf0ed6376953e2e4f8d81dba0350a
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-04-19 19:33:28 +1100

    'first'

--- task2.cpp
+++ task2.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+int var(int jump, int step_number, int var) {
+
+	if (jump == step_number) {
+
+		return var;
+
+	}
+
+	for (int i = 0; i < step_number; ++i) {
+
+
+
+	}
+
+}
+
+
+
 int main() {
 
-
+	int jump = 0;
+	int step_number = 3
+	int var = 0;
 
 }
